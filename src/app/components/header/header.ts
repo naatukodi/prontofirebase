a7ce7548c756128ed81d268e1cb5be5a2a687cc0
Tirumala Rao  S <tiru@live.in>
2025-06-15 22:09:43 +0530

    fixed menu css....still needs fixing

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,34 +1,33 @@
 // src/app/header.component.ts
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule, MatSidenav }       from '@angular/material/sidenav';
 import { MatButtonModule }  from '@angular/material/button';
+import { MatIconModule }    from '@angular/material/icon';
+import { MatMenuModule }    from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+import { BreakpointObserver }     from '@angular/cdk/layout';
 import { RouterModule }     from '@angular/router';
 import { AuthService }      from '../../services/auth.service';
 import { AuthorizationService } from '../../services/authorization.service';
 import { Auth, authState, User } from '@angular/fire/auth';
-import { Observable }       from 'rxjs';
+import { Observable, map }       from 'rxjs';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, RouterModule],
-  template: `
-    <mat-toolbar color="primary" class="app-toolbar">
-      <span class="app-title">Pronto</span>
-      <span class="spacer"></span>
-      <button mat-button routerLink="/">Dashboard</button>
-      <button mat-button routerLink="/stakeholder">Stakeholder</button>
-
-      <!-- only show Logout when user is signed in -->
-      <button
-        mat-button
-        (click)="onLogout()">
-        Logout
-      </button>
-    </mat-toolbar>
-  `
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, MatDividerModule, RouterModule, MatSidenavModule, MatListModule, CommonModule],
+  templateUrl: './header.html',
+  styleUrls: ['./header.css']
 })
 export class HeaderComponent {
+  @ViewChild('drawer') drawer!: MatSidenav;
+  private bp = inject(BreakpointObserver);
+  isHandset$: Observable<boolean> = this.bp
+    .observe(['(max-width: 768px)'])
+    .pipe(map(r => r.matches));
   authSvc = inject(AuthService);
   user$: Observable<User | null> = authState(inject(Auth));
   authzSvc1 = inject(AuthorizationService);
@@ -38,3 +37,4 @@ export class HeaderComponent {
     this.authzSvc1.clearPermissions();
   }
 }
+
